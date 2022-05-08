f9ad09877a820b7266573e89ed961ee8ca67e38c
Vicencio42 <vicencio42@gmail.com>
2022-05-08 10:12:13 -0500

    added bcrypt gem, added user admin boolean, added validates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_29_015530) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_08_144815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_29_015530) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
   end
 
   create_table "wishlists", force: :cascade do |t|
